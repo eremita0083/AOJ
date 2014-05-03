b2851403b215b97d4bdb363e95c64b557d4cc817
eremita0083 <schrodinger.s.cat0083@gmail.com>
2014-05-03 23:37:35 +0900

    100_1

--- src/Volume100_1.java
+++ src/Volume100_1.java
@@ -1,12 +1,8 @@
 
 public class Volume100_1 {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello World");
 	}
 
 }
