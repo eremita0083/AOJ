a693963b5878e730e6a017c2aa4200275bce5db0
eremita0083 <schrodinger.s.cat0083@gmail.com>
2014-05-05 02:42:24 +0900

    10007

---
+++ src/Volume100_7.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Volume100_7 {
+	private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws IOException {
+		for(int i =1 ;; i++){
+			String[] array = reader.readLine().split(" ");
+			int x = Integer.parseInt(array[0]);
+			int y = Integer.parseInt(array[1]);
+			if(x == 0 && y == 0 ){
+				break;
+			}else if(Math.max(x, y) != x){
+				System.out.println(x +" "+ y);
+			}else{
+				System.out.println(y +" "+ x);
+			}
+		}
+	}
+}
