9efab0f2379e791ea0246004e0458d6800b07506
eremita0083 <schrodinger.s.cat0083@gmail.com>
2014-05-04 23:20:40 +0900

    100_2 rectangle

---
+++ src/Volume100_3.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Volume100_3 {
+
+	public static void main(String[] args) throws IOException {
+		java.io.BufferedReader reader = new java.io.BufferedReader(
+                new java.io.InputStreamReader(System.in));
+        String input = reader.readLine();
+        String array[] = (input.split(" "));
+        int height = Integer.parseInt(array[0]);
+        int width = Integer.parseInt(array[1]);
+        System.out.println(height*width + " " + (height+width)*2);
+	}
+
+}
