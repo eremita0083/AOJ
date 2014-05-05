daf31912d19a85df427e9a4a23b84a315b5803cd
eremita0083 <schrodinger.s.cat0083@gmail.com>
2014-05-05 19:44:33 +0900

    10011

---
+++ src/Volume100_11.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Volume100_11 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int count = Integer.parseInt(reader.readLine());
+		String[] array = reader.readLine().split(" ");
+		String[] sortArray = new String[count];
+		for(int i = 0; i < count ; i++){
+			sortArray[i] = array[count-i-1];
+		}
+		StringBuilder builder = new StringBuilder();
+		for(String str : sortArray){
+			builder.append(str+" ");
+		}
+		String result = builder.toString();
+		System.out.println(result.trim());
+	}
+
+}
