f0039fb71c3e4a97168eaf51034aba04ac4042ad
eremita0083 <schrodinger.s.cat0083@gmail.com>
2014-05-05 20:15:37 +0900

    10012

---
+++ src/Volume100_12.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Volume100_12 {
+	private static final BufferedReader M_READER = new BufferedReader(
+			new InputStreamReader(System.in));
+	private static final String CELL = "#";
+
+	public static void main(String[] args) throws IOException {
+		while(true){
+			String[] array = M_READER.readLine().split(" ");
+			int height = Integer.parseInt(array[0]);
+			int width = Integer.parseInt(array[1]);
+			if(height ==0 && width ==0 ){
+				return;
+			}
+			StringBuilder builder = new StringBuilder();
+			for(int i = 0; i < height ; i++){
+				for(int j = 0 ; j < width ; j++){
+					builder.append(CELL);
+				}
+				builder.append("\n");
+			}
+			System.out.println(builder.toString());
+		}
+	}
+}
