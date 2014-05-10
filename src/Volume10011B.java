91f2b9dd84ec1379487abeaeb76d1a8ba741d382
eremita0083 <schrodinger.s.cat0083@gmail.com>
2014-05-10 16:06:42 +0900

    10011B追加

---
+++ src/Volume10011B.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Volume10011B {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		Integer.parseInt(reader.readLine());
+		String[] array = reader.readLine().split(" ");
+		for(int i = array.length-1; i >= 0 ; i--){
+			if(i == 0) {
+				System.out.print(array[i]); 
+			}else{
+				System.out.print(array[i]+" ");
+			}
+		}
+		System.out.println("");
+	}
+
+}
