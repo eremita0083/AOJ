d97b07acdef3a56db392d369ab10435792f849ac
eremita0083 <schrodinger.s.cat0083@gmail.com>
2014-05-16 22:27:02 +0900

    10031, 10032

--- src/Volume10031.java
+++ src/Volume10031.java
@@ -10,37 +10,38 @@ public class Volume10031 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(
 				System.in));
-		reader.readLine();
+		int sCount = Integer.parseInt(reader.readLine());
 		String[] s = reader.readLine().split(" ");
-		reader.readLine();
+		int qCount = Integer.parseInt(reader.readLine());
 		String[] q = reader.readLine().split(" ");
-		Map<String, Integer> map = new HashMap<String, Integer>();
-		
-		int counter = 0;
-	    Arrays.sort(s);
-	    for (int i = 0; i < q.length; i++) {
-	    	if (Arrays.binarySearch(s, q[i]) >= 0) {
-	    		counter++;
-	        }
-	    }
-		System.out.println(map.size());
+		int count = 0;
+		int minCount = Math.min(sCount, qCount);
+		if (minCount == qCount) {
+			Arrays.sort(s);
+			for (int i = 0; i < minCount; i++) {
+				if (Arrays.binarySearch(s, q[i]) >= 0) {
+					count++;
+				}
+			}
+		}else {
+			Arrays.sort(q);
+			for (int i = 0; i < minCount; i++) {
+				if (Arrays.binarySearch(q, s[i]) >= 0) {
+					count++;
+				}
+			}
+		}
+		System.out.println(count);
 	}
 }
-/*BufferedReader reader = new BufferedReader(new InputStreamReader(
-        System.in));
-int sCount = Integer.parseInt(reader.readLine());
-String[] s = reader.readLine().split(" ");
-int qCount = Integer.parseInt(reader.readLine());
-String[] q = reader.readLine().split(" ");
-Map<String, Integer> map = new HashMap<String, Integer>();
-int minCount = Math.min(sCount,qCount);
-for (String sStr : s) {
-    if (minCount > Integer.parseInt(sStr)) {
-        for (String qstr : q) {
-            if (sStr.equals(qstr)) {
-                map.put(qstr, null);
-            }
-        }
-    }
-}
-System.out.println(map.size());*/
+/*
+ * BufferedReader reader = new BufferedReader(new InputStreamReader(
+ * System.in)); int sCount = Integer.parseInt(reader.readLine()); String[] s =
+ * reader.readLine().split(" "); int qCount =
+ * Integer.parseInt(reader.readLine()); String[] q =
+ * reader.readLine().split(" "); Map<String, Integer> map = new HashMap<String,
+ * Integer>(); int minCount = Math.min(sCount,qCount); for (String sStr : s) {
+ * if (minCount > Integer.parseInt(sStr)) { for (String qstr : q) { if
+ * (sStr.equals(qstr)) { map.put(qstr, null); } } } }
+ * System.out.println(map.size());
+ */
